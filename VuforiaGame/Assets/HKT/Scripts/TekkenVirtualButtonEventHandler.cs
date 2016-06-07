f34b5792edb9e257fca28f118fcddb04dca23b65
Tony Sparrow <hoamgminh.nguyen.vn@gmail.com>
2016-06-07 11:32:35 +0700

    - replace old plugin, start implement new

--- VuforiaGame/Assets/HKT/Scripts/TekkenVirtualButtonEventHandler.cs
+++ VuforiaGame/Assets/HKT/Scripts/TekkenVirtualButtonEventHandler.cs
@@ -1,61 +1,62 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public class TekkenVirtualButtonEventHandler : MonoBehaviour, IVirtualButtonEventHandler
+public class TekkenVirtualButtonEventHandler : MonoBehaviour
 {
-		Animation anim;
-		List<AnimationState> animState;
-		private int index;
-		public GameObject imgTarget;
-		public void OnCreateAnimationPlay ()
-		{
+	Animation anim;
+	List<AnimationState> animState;
+	private int index;
+	public GameObject imgTarget;
 
-				//Debug.Log (obj.name);
-				animState = new List<AnimationState> ();
+	public void OnCreateAnimationPlay ()
+	{
+
+		//Debug.Log (obj.name);
+		animState = new List<AnimationState> ();
 //				Debug.Log (gameObject.name);
 //				Debug.Log (gameObject.transform.GetChild (1).name);
-				anim = imgTarget.transform.GetChild (1).GetComponent<Animation> ();
-				foreach (AnimationState clip in anim) {
-						animState.Add (clip);
-				}
-				index = 0;
-				anim.Play (animState [index].name);
-				Debug.Log (animState.Count);
-				Debug.Log (animState [index].name);
-
+		anim = imgTarget.transform.GetChild (1).GetComponent<Animation> ();
+		foreach (AnimationState clip in anim) {
+			animState.Add (clip);
 		}
-	
-		/// <summary>
-		/// Called when the scene is loaded
-		/// </summary>
-		void Start ()
-		{
-				// Search for all Children from this ImageTarget with type VirtualButtonBehaviour
+		index = 0;
+		anim.Play (animState [index].name);
+		Debug.Log (animState.Count);
+		Debug.Log (animState [index].name);
+
+	}
+
+	/// <summary>
+	/// Called when the scene is loaded
+	/// </summary>
+	void Start ()
+	{
+		// Search for all Children from this ImageTarget with type VirtualButtonBehaviour
 //				VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour> ();
 //				for (int i = 0; i < vbs.Length; ++i) {
 //						// Register with the virtual buttons TrackableBehaviour
 //						vbs [i].RegisterEventHandler (this);
 //						vbs [i].UpdateAreaRectangle ();
 //				}
+	}
+
+	/// <summary>
+	/// Called when the virtual button has just been pressed:
+	/// </summary>
+	public void OnButtonPressed ()
+	{
+		if (index < animState.Count - 1) {
+			index++;
+		} else {
+			index = 0;
 		}
-	
-		/// <summary>
-		/// Called when the virtual button has just been pressed:
-		/// </summary>
-		public void OnButtonPressed (VirtualButtonAbstractBehaviour vb)
-		{
-				if (index < animState.Count - 1) {
-						index++;
-				} else {
-						index = 0;
-				}
-				anim.Play (animState [index].name);
-		}
-	
-		/// <summary>
-		/// Called when the virtual button has just been released:
-		/// </summary>
-		public void OnButtonReleased (VirtualButtonAbstractBehaviour vb)
-		{
-		}
+		anim.Play (animState [index].name);
+	}
+
+	/// <summary>
+	/// Called when the virtual button has just been released:
+	/// </summary>
+	public void OnButtonReleased ()
+	{
+	}
 }
